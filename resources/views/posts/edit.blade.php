23cf949adb54f46e278584bf06fb9f5940aafa9b
elmoplug <justaway3474@gmail.com>
2023-08-18 08:30:31 +0900

    9-3

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE HTML>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-    </head>
-    
-    <body>
+
+    <x-app-layout>
         <h1 class="title">編集画面</h1>
         <div class="content">
             <form action="/posts/{{ $post->id }}" method="POST">
@@ -22,6 +16,5 @@
                 <input type="submit" value="保存">
             </form>
         </div>
-    </body>
+    </x-app-layout>
     
-</html>
